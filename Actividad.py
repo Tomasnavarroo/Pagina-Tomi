4f11295a512802cafd082fca73a3d023fe076d4c
Tomasnavarroo <tanavarro@escuelasproa.edu.ar>
2024-11-11 09:56:30 -0300

    Add files via upload

---
+++ Actividad.py
@@ -0,0 +1,14 @@
+#Crear una clase animal,definir sus atributos e inicializarlos en la función constructora y luego crear 4 objetos de esa clase
+class animal:
+    def__init__(self,nombre,edad,especie):
+      self.edad=edad
+      self.especie=especie
+      self.nombre=nombre
+  
+  #Crear 4 objetos de la clase
+
+animal1=animal("Serpiente",20,"Reptil")   
+animal2=animal("Ballena",90,"Cetaceo")
+animal3=animal("Murcielago",30,"Mamifero")
+animal4=animal("Sapo",12,"Anfibio")
+
